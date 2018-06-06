9a5f5c998232d33892eb418da52e06c6849f7451
07JP27 <jynpei.t@gmail.com>
2018-06-06 13:36:30 +0900

    Initial commit

---
+++ Xamarin-SQLite-Sample/Xamarin-SQLite-SamplePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinSQLiteSample
+{
+    public partial class Xamarin_SQLite_SamplePage : ContentPage
+    {
+        public Xamarin_SQLite_SamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
